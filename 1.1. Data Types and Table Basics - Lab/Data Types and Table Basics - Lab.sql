fbbce02795917fb47e7a53e8e08db7ec7ea08dce
Borislav Ivanov <slambeca@abv.bg>
2023-09-19 14:01:33 +0300

    Add files via upload

---
+++ 1.1. Data Types and Table Basics - Lab/Data Types and Table Basics - Lab.sql
@@ -0,0 +1,57 @@
+00. Create Database
+
+CREATE DATABASE gamebar;
+
+01. Create Tables
+
+CREATE TABLE
+	employees(
+		id SERIAL PRIMARY KEY NOT NULL,
+		first_name VARCHAR(30),
+		last_name VARCHAR(50),
+		hiring_date DATE DEFAULT '2023-01-01',
+		salary NUMERIC(10, 2),
+		devices_number INT		
+	);
+	
+CREATE TABLE
+	departments(
+		id SERIAL PRIMARY KEY NOT NULL,
+		"name" VARCHAR(50),
+		code CHAR(3),
+		description TEXT
+	);
+	
+CREATE TABLE
+	issues(
+		id SERIAL PRIMARY KEY UNIQUE,
+		description VARCHAR(150),
+		"date" DATE,
+		start TIMESTAMP
+	);
+
+03. Alter Tables
+
+ALTER TABLE employees
+
+ADD COLUMN middle_name VARCHAR(50);
+
+04. Add Constraints
+
+ALTER TABLE employees
+ALTER COLUMN salary SET NOT NULL,
+ALTER COLUMN salary SET DEFAULT 0,
+ALTER COLUMN hiring_date SET NOT NULL;
+
+05. Modify Columns
+
+ALTER TABLE employees
+ALTER COLUMN middle_name TYPE VARCHAR(100);
+
+06. Truncate Tables
+
+TRUNCATE TABLE issues;
+
+07. Drop Tables
+
+DROP TABLE departments;
