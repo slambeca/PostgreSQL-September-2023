35b2c638ffb177ef438e5da0c62f630a6a747011
Borislav Ivanov <slambeca@abv.bg>
2023-09-20 13:30:16 +0300

    Update Data Types and Table Basics - Exercise.sql

--- 1.2. Data Types and Table Basics - Exercise/Data Types and Table Basics - Exercise.sql
+++ 1.2. Data Types and Table Basics - Exercise/Data Types and Table Basics - Exercise.sql
@@ -54,3 +54,68 @@ ALTER TABLE minions_info
 ALTER COLUMN age SET DEFAULT 0,
 ALTER COLUMN "name" SET DEFAULT '',
 ALTER COLUMN code SET DEFAULT '';
+
+08. Add Constraints
+
+-- ALTER TABLE minions_info
+-- ADD CONSTRAINT UQ_email_and_id UNIQUE (id, email),
+-- ADD CONSTRAINT CK_banana_is_positive_number CHECK(banana>0);
+
+ALTER TABLE minions_info
+ADD CONSTRAINT unique_containt UNIQUE (id, email),
+ADD CONSTRAINT banana_check CHECK(banana>0);
+
+09. Change Column’s Data Type
+
+ALTER TABLE minions_info
+ALTER COLUMN task TYPE VARCHAR(150);
+
+10. Drop Constraint
+
+ALTER TABLE minions_info
+ALTER COLUMN equipped DROP NOT NULL;
+
+11. Remove Column
+
+ALTER TABLE minions_info
+DROP COLUMN age;
+
+12. Table Birthdays
+
+CREATE TABLE minions_birthdays (
+	id INTEGER UNIQUE NOT NULL,
+	"name" VARCHAR(50),
+	date_of_birth DATE,
+	age INTEGER,
+	present VARCHAR(100),
+	party TIMESTAMPTZ
+);
+
+13. Insert Into
+
+INSERT INTO minions_info 
+	(name, code, task, banana, email, equipped, mood)
+VALUES 
+	('Mark', 'GKYA', 'Graphing Points', 3265.265, 'mark@minion.com', false, 'happy'),
+	('Mel', 'HSK', 'Science Investigation', 54784.996, 'mel@minion.com', true, 'stressed'),
+	('Bob', 'HF', 'Painting', 35.652, 'bob@minion.com', true, 'happy'),
+	('Darwin', 'EHND', 'Create a Digital Greeting', 321.958, 'darwin@minion.com', false, 'relaxed'),
+	('Kevin', 'KMHD', 'Construct with Virtual Blocks', 35214.789, 'kevin@minion.com', false, 'happy'),
+	('Norbert', 'FEWB', 'Testing', 3265.500, 'norbert@minion.com', true, 'sad'),
+	('Donny', 'L', 'Make a Map', 8.452, 'donny@minion.com', true, 'happy');
+
+14. Select
+
+SELECT name, task, email, banana FROM minions_info;
+
+15. Truncate the Table
+
+TRUNCATE TABLE minions_info;
+
+16. Drop the Table
+
+DROP TABLE minions_birthdays;
+
+17. Drop the Database
+
+DROP DATABASE minions_db WITH (FORCE);
