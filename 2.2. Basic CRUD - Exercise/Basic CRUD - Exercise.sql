278dbc0d0b80ccddba53227c495cc6e429b1da13
Borislav Ivanov <slambeca@abv.bg>
2023-09-22 18:41:44 +0300

    Update Basic CRUD - Exercise.sql

--- 2.2. Basic CRUD - Exercise/Basic CRUD - Exercise.sql
+++ 2.2. Basic CRUD - Exercise/Basic CRUD - Exercise.sql
@@ -1,4 +1,27 @@
 -- 01. Select Cities 
 
 SELECT * FROM cities
+
 ORDER BY id ASC;
+
+-- 02. Concatenate
+
+SELECT CONCAT(name, ' ', state) AS "Cities Information",
+area AS "Area (km2)"
+FROM cities;
+
+-- 03. Remove Duplicate Rows
+
+SELECT DISTINCT name, 
+area AS "Area (km2)"
+FROM cities
+ORDER BY NAME DESC;
+
+-- 04. Limit Records
+
+SELECT id AS "ID",
+CONCAT(first_name, ' ', last_name) AS "Full Name",
+job_title AS "Job Title"
+FROM employees
+ORDER BY first_name
+LIMIT 50;  
