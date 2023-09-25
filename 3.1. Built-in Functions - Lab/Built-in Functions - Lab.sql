49a381316c5cdcba7ec178ec7fc154480c16dee1
Borislav Ivanov <slambeca@abv.bg>
2023-09-25 15:13:25 +0300

    Update Built-in Functions - Lab.sql

--- 3.1. Built-in Functions - Lab/Built-in Functions - Lab.sql
+++ 3.1. Built-in Functions - Lab/Built-in Functions - Lab.sql
@@ -1,13 +1,18 @@
 -- 01. Find Book Titles 
 
 SELECT 
-	
 	title 
-
 FROM 
-	
 	books
-
 WHERE 
-	
 	title LIKE 'The%';
+
+-- Variant 2 with SUBSTRING
+SELECT 
+	title 
+FROM 
+	books
+WHERE 
+	SUBSTRING(title, 1, 3) = 'The'
+ORDER BY
+	id ASC;
