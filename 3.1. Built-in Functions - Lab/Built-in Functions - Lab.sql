9504bce07f90011a6f29ff4cede80df3b0a8bc0c
Borislav Ivanov <slambeca@abv.bg>
2023-09-25 22:12:44 +0300

    Update Built-in Functions - Lab.sql

--- 3.1. Built-in Functions - Lab/Built-in Functions - Lab.sql
+++ 3.1. Built-in Functions - Lab/Built-in Functions - Lab.sql
@@ -5,14 +5,35 @@ SELECT
 FROM 
 	books
 WHERE 
-	title LIKE 'The%';
+	SUBSTRING(title, 1, 3) = 'The'
+ORDER BY
+	id ASC;
 
--- Variant 2 with SUBSTRING
+# Variant 2 with LIKE
+
 SELECT 
 	title 
 FROM 
 	books
 WHERE 
-	SUBSTRING(title, 1, 3) = 'The'
+	title LIKE 'The%';
+
+-- 02. Replace Titles 
+
+SELECT
+	REPLACE(title, 'The', '***')
+FROM
+	books
+WHERE title LIKE 'The%'
 ORDER BY
+	id ASC;
+
+-- 03. Triangles on Bookshelves
+
+SELECT
+	id,
+	side * height / 2 AS area
+FROM
+	triangles
+ORDER BY 
 	id ASC;
