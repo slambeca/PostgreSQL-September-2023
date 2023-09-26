6b300c4e49249bab73edaae73565b8d049bf34e6
Borislav Ivanov <slambeca@abv.bg>
2023-09-26 22:16:20 +0300

    Update Built-in Functions - Exercise.sql

--- 3.2. Built-in Functions - Exercise/Built-in Functions - Exercise.sql
+++ 3.2. Built-in Functions - Exercise/Built-in Functions - Exercise.sql
@@ -145,4 +145,157 @@ SELECT
 FROM
 	countries;
 
--- 13. Positive and Negative LEFT 
+-- 13. Positive and Negative LEFT
+
+SELECT
+	peak_name,
+	LEFT(peak_name, 4) AS "Positive Left",
+	LEFT(peak_name, -4) AS "Negative Right"
+FROM
+	peaks;
+
+-- 14. Positive and Negative RIGHT
+
+SELECT
+	peak_name,
+	RIGHT(peak_name, 4) AS "Positive Right",
+	RIGHT(peak_name, -4) AS "Negative Right"
+FROM
+	peaks;
+
+-- 15. Update iso_code
+
+UPDATE
+	countries
+SET
+	iso_code = UPPER(LEFT(country_name, 3))
+WHERE
+	iso_code IS NULL;
+
+-- 16. REVERSE country_code
+
+UPDATE 
+	countries
+SET 
+	country_code = LOWER(REVERSE(country_code));
+
+17. Elevation --->> Peak Name
+
+SELECT
+	CONCAT_WS(' --->> ', elevation, peak_name) AS "Elevation --->> Peak Name"
+FROM
+	peaks
+WHERE elevation >= 4884;
+
+-- Variant 2
+
+SELECT
+	CONCAT_WS(' ', elevation, REPEAT('-', 3) || REPEAT('>', 2), peak_name) AS "Elevation --->> Peak Name"
+FROM
+	peaks
+WHERE elevation >= 4884;
+
+-- 18. Arithmetical Operators
+
+CREATE TABLE
+	bookings_calculation
+AS
+SELECT
+	booked_for,
+	booked_for::numeric * 50 AS multiplication,
+	booked_for::numeric % 50 AS modulo
+FROM
+	bookings
+WHERE
+	apartment_id = 93;
+
+-- 19. ROUND vs TRUNC
+
+SELECT
+	latitude,
+	ROUND(latitude, 2) AS "round",
+	TRUNC(latitude, 2) AS "trunc"
+FROM
+	apartments;
+
+-- 20. Absolute Value
+
+SELECT
+	longitude,
+	ABS(longitude) as "abs"
+FROM
+	apartments;
+
+-- 21. Billing Day** Cannot be tested in Judge.
+
+ALTER TABLE
+	bookings
+ADD COLUMN
+	billing_day TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP;
+	
+SELECT 
+	TO_CHAR(billing_day, 'DD "Day" MM "Month" YYYY "Year" HH24:MI:SS') AS "Billing Day"
+FROM
+	bookings;
+
+-- 22. EXTRACT Booked At
+
+SELECT
+	EXTRACT(YEAR FROM booked_at) AS YEAR,
+	EXTRACT(MONTH FROM booked_at) AS MONTH,
+	EXTRACT(DAY FROM booked_at) AS DAY,
+	EXTRACT(HOUR FROM booked_at AT TIME ZONE 'UTC') AS HOUR,
+	EXTRACT(MINUTE FROM booked_at) AS MINUTE,
+	CEIL(EXTRACT(SECOND FROM booked_at)) AS SECOND
+FROM
+	bookings;
+
+-- 23. Early Birds** Cannot be tested in Judge.
+
+SELECT
+	user_id,
+	AGE(start_date, booked_at) AS "Early Birds"
+FROM
+	bookings
+WHERE
+	start_date - booked_at >= '10 MONTHS';
+
+-- 24. Match or Not
+
+SELECT
+	companion_full_name,
+	email
+FROM
+	users
+WHERE
+	companion_full_name ILIKE '%aNd%'
+		AND
+	email NOT LIKE '%@gmail';
+
+-- 25. COUNT by Initial
+
+SELECT
+	LEFT(first_name, 2) AS initials,
+	COUNT('initials') AS user_count
+FROM
+	users
+GROUP BY 
+	initials
+ORDER BY
+	user_count DESC,
+	initials ASC;
+
+-- 26. SUM
+
+SELECT
+	SUM(booked_for) AS total_value
+FROM
+	bookings
+WHERE apartment_id = 90;
+
+-- 27. Average Value
+
+SELECT
+	AVG(multiplication) AS average_value
+FROM
+	bookings_calculation;
