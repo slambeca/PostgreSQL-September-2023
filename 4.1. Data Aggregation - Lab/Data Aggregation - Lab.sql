d6a4e54f4d4157acb0f9b7e485e531ab904b7670
Borislav Ivanov <slambeca@abv.bg>
2023-09-27 14:31:59 +0300

    Update Data Aggregation - Lab.sql

--- 4.1. Data Aggregation - Lab/Data Aggregation - Lab.sql
+++ 4.1. Data Aggregation - Lab/Data Aggregation - Lab.sql
@@ -1,3 +1,5 @@
+-- 01. Departments Info by ID 
+
 SELECT
 	department_id,
 	COUNT(id) AS employee_count
@@ -7,3 +9,94 @@ GROUP BY
 	department_id
 ORDER BY
 	department_id;
+
+-- 02. Departments Info by Salary
+
+SELECT
+	department_id,
+	COUNT(salary) AS employee_count
+FROM
+	employees
+GROUP BY
+	department_id
+ORDER BY
+	department_id;
+
+-- 03. Sum Salaries per Department
+
+SELECT
+	department_id,
+	SUM(salary)
+FROM
+	employees
+GROUP BY
+	department_id
+ORDER BY
+	department_id;
+
+-- 04. Maximum Salary 
+
+SELECT
+	department_id,
+	MAX(salary) AS max_salary
+FROM
+	employees
+GROUP BY
+	department_id
+ORDER BY
+	department_id;
+
+-- 05. Minimum Salary 
+
+SELECT
+	department_id,
+	MIN(salary) AS min_salary
+FROM
+	employees
+GROUP BY
+	department_id
+ORDER BY
+	department_id;
+
+-- 06. Average Salary
+
+SELECT
+	department_id,
+	AVG(salary) AS avg_salary
+FROM
+	employees
+GROUP BY
+	department_id
+ORDER BY
+	department_id;
+
+-- 07. Filter Total Salaries
+
+SELECT
+	department_id,
+	SUM(salary) AS total_salary
+FROM
+	employees
+GROUP BY
+	department_id
+HAVING 
+	SUM(salary) < 4200
+ORDER BY
+	department_id;
+
+-- 08. Department Names
+
+SELECT
+	id,
+	first_name,
+	last_name,
+	salary,
+	department_id,
+	CASE
+		WHEN department_id = 1 THEN 'Management'
+		WHEN department_id = 2 THEN 'Kitchen Staff'
+		WHEN department_id = 3 THEN 'Service Staff'
+		WHEN department_id > 3 THEN 'Other'
+	END AS department_name
+FROM
+	employees;
