3663d22d3f1040cb64823742166a8336ff77a8e7
Borislav Ivanov <slambeca@abv.bg>
2023-10-03 16:03:05 +0300

    Update Table Relations - Lab.sql

--- 5.1. Table Relations - Lab/Table Relations - Lab.sql
+++ 5.1. Table Relations - Lab/Table Relations - Lab.sql
@@ -42,3 +42,20 @@ ON
 	c.id = r.leader_id;
 
 -- 4. Delete Mountains
+
+CREATE TABLE
+	mountains (
+		id SERIAL PRIMARY KEY,
+		name VARCHAR(50)
+	);
+	
+CREATE TABLE
+	peaks (
+		id SERIAL PRIMARY KEY,
+		name VARCHAR(50),
+		mountain_id INT,
+		CONSTRAINT fk_mountain_id
+		FOREIGN KEY (mountain_id)
+		REFERENCES mountains(id)
+		ON DELETE CASCADE	
+	);
