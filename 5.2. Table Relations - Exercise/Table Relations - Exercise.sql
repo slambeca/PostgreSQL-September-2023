ecd3a15178332baf7c2267e70adea3e5f9de4c59
Borislav Ivanov <slambeca@abv.bg>
2023-10-04 14:01:27 +0300

    Update Table Relations - Exercise.sql

--- 5.2. Table Relations - Exercise/Table Relations - Exercise.sql
+++ 5.2. Table Relations - Exercise/Table Relations - Exercise.sql
@@ -110,3 +110,174 @@ VALUES
 	('1916-03-01', 1),
 	('2003-01-01', 2),
 	('1966-05-01', 3);
+
+-- 05. See ERD Diagram (can not be tested in Judge)
+
+-- 06. Photo Shooting
+
+CREATE TABLE IF NOT EXISTS
+	customers (
+		id SERIAL PRIMARY KEY,
+		name VARCHAR(50),
+		date DATE
+);
+
+CREATE TABLE IF NOT EXISTS
+	photos (
+		id SERIAL PRIMARY KEY,
+		url VARCHAR(50),
+		place VARCHAR(50),
+		customer_id INT,
+		CONSTRAINT fk_photos_customers
+			FOREIGN KEY (customer_id)
+				REFERENCES customers(id)
+);
+
+INSERT INTO
+	customers(name, date)
+VALUES
+	('Bella', '2022-03-25'),
+	('Philip', '2022-07-05');
+	
+INSERT INTO
+	photos(url, place, customer_id)
+VALUES
+	('bella_1111.com',	'National Theatre',	1),
+	('bella_1112.com', 'Largo',	1),
+	('bella_1113.com',	'The View Restaurant',	1),
+	('philip_1121.com',	'Old Town',	2),
+	('philip_1122.com',	'Rowing Canal',	2),
+	('philip_1123.com',	'Roman Theater', 2);
+
+-- 07. See ERD Diagram (can not be tested in Judge)
+
+-- 08. Study Session
+
+CREATE TABLE IF NOT EXISTS
+	students (
+		id SERIAL PRIMARY KEY,
+		student_name VARCHAR(50)
+);
+
+CREATE TABLE IF NOT EXISTS
+	exams (
+		id INT GENERATED ALWAYS AS IDENTITY (START 101 INCREMENT 1) PRIMARY KEY,
+		exam_name VARCHAR(50)
+);
+
+CREATE TABLE IF NOT EXISTS
+	study_halls (
+		id SERIAL PRIMARY KEY,
+		study_hall_name VARCHAR(50),
+		exam_id INT,
+		CONSTRAINT fk_study_halls_exams
+			FOREIGN KEY (exam_id)
+				REFERENCES exams(id)
+);
+
+CREATE TABLE IF NOT EXISTS
+	students_exams (
+		student_id INT,
+		exam_id INT,
+		CONSTRAINT pk_students_exams
+			PRIMARY KEY (student_id, exam_id),
+		CONSTRAINT fk_students_exams_students
+			FOREIGN KEY (student_id)
+				REFERENCES students(id),
+		CONSTRAINT fk_students_exams_exams
+			FOREIGN KEY (exam_id)
+				REFERENCES exams(id)
+);
+
+INSERT INTO
+	students(student_name)
+VALUES
+	('Mila'),
+	('Toni'),
+	('Ron');
+	
+INSERT INTO
+	exams(exam_name)
+VALUES
+	('Python Advanced'),
+	('Python OOP'),
+	('PostgreSQL');
+	
+INSERT INTO
+	study_halls(study_hall_name, exam_id)
+VALUES
+	('Open Source Hall', 102),
+	('Inspiration Hall', 101),
+	('Creative Hall', 103),
+	('Masterclass Hall', 103),
+	('Information Security Hall', 103);
+	
+INSERT INTO
+	students_exams(student_id, exam_id)
+VALUES
+	(1, 101),
+	(1, 102),
+	(2, 101),
+	(3, 103),
+	(2, 102),
+	(2, 103);
+
+-- 09. See ERD Diagram (can not be tested in Judge)
+
+-- 10. Online Store
+
+CREATE TABLE IF NOT EXISTS
+	item_types (
+		id SERIAL PRIMARY KEY,
+		item_type_name VARCHAR(50)
+);
+
+CREATE TABLE IF NOT EXISTS
+	cities (
+		id SERIAL PRIMARY KEY,
+		city_name VARCHAR(50)
+);
+
+CREATE TABLE IF NOT EXISTS
+	items (
+		id SERIAL PRIMARY KEY,
+		item_name VARCHAR(50),
+		item_type_id INT,
+		CONSTRAINT fk_items_item_types
+			FOREIGN KEY (item_type_id)
+				REFERENCES item_types(id)
+);
+
+CREATE TABLE IF NOT EXISTS
+	customers (
+		id SERIAL PRIMARY KEY,
+		customer_name VARCHAR(50),
+		birthday DATE,
+		city_id INT,
+		CONSTRAINT fk_customers_cities
+			FOREIGN KEY (city_id)
+				REFERENCES cities(id)
+);
+
+CREATE TABLE IF NOT EXISTS
+	orders (
+		id SERIAL PRIMARY KEY,
+		customer_id INT,
+		CONSTRAINT fk_orders_customers
+			FOREIGN KEY (customer_id)
+				REFERENCES customers(id)
+);
+
+CREATE TABLE IF NOT EXISTS
+	order_items (
+		order_id INT,
+		item_id INT,
+		CONSTRAINT pk_order_items
+			PRIMARY KEY (order_id, item_id),
+		CONSTRAINT fk_order_items_orders
+			FOREIGN KEY (order_id)
+				REFERENCES orders(id),
+		CONSTRAINT fk_order_items_items
+			FOREIGN KEY (item_id)
+				REFERENCES items(id)
+);
