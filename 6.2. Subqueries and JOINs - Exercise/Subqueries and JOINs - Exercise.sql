c248c85438caaab5b675afa46a42cf87d9ec7578
Borislav Ivanov <slambeca@abv.bg>
2023-10-05 01:34:31 +0300

    Update Subqueries and JOINs - Exercise.sql

--- 6.2. Subqueries and JOINs - Exercise/Subqueries and JOINs - Exercise.sql
+++ 6.2. Subqueries and JOINs - Exercise/Subqueries and JOINs - Exercise.sql
@@ -1,23 +1,13 @@
 -- 01. Booked for Nights
 
 SELECT
-	
 	CONCAT_WS(' ', a.address, a.address_2) AS apartment_address,
-	
 	b.booked_for AS nights
-
 FROM
-	
 	apartments AS a
-
 JOIN
-	
 	bookings AS b
-
 USING
-	
 	(booking_id)
-
 ORDER BY
-	
 	a.apartment_id ASC;
