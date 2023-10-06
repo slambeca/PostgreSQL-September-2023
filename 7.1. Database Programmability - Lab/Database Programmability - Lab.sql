42740584e0ec0f4dd35eaacf79cf2780e92fb412
Borislav Ivanov <slambeca@abv.bg>
2023-10-06 22:24:24 +0300

    Update Database Programmability - Lab.sql

--- 7.1. Database Programmability - Lab/Database Programmability - Lab.sql
+++ 7.1. Database Programmability - Lab/Database Programmability - Lab.sql
@@ -45,3 +45,21 @@ END;
 $$;
 
 CALL sp_increase_salaries('Finance')
+
+-- 03. Employees Promotion By ID
+
+CREATE PROCEDURE sp_increase_salary_by_id(id INT)
+AS
+$$
+BEGIN
+	IF (SELECT salary FROM employees WHERE employee_id = id) IS NULL THEN
+		RETURN;
+	ELSE
+		UPDATE employees SET salary = salary * 1.05 WHERE employee_id = id;
+	END IF;
+	COMMIT;
+END;
+$$
+LANGUAGE plpgsql;
+
+-- 04. Triggered
