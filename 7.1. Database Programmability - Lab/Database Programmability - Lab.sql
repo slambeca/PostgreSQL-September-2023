f90fbfbd847bcae524f606bcafca842a2355494d
Borislav Ivanov <slambeca@abv.bg>
2023-10-09 14:03:30 +0300

    Update Database Programmability - Lab.sql

--- 7.1. Database Programmability - Lab/Database Programmability - Lab.sql
+++ 7.1. Database Programmability - Lab/Database Programmability - Lab.sql
@@ -63,3 +63,40 @@ $$
 LANGUAGE plpgsql;
 
 -- 04. Triggered
+
+CREATE TABLE 
+	deleted_employees (
+      employee_id SERIAL PRIMARY KEY,
+      first_name VARCHAR(20),
+      last_name VARCHAR(20),
+      middle_name VARCHAR(20),
+      job_title VARCHAR(50),
+      department_id INT,
+      salary NUMERIC(19,4)
+);
+
+CREATE OR REPLACE FUNCTION 
+	on_employee_delete()
+RETURNS TRIGGER AS 
+$$
+	BEGIN
+		INSERT INTO deleted_employees (
+			first_name, 
+			last_name, 
+			middle_name, 
+			job_title, 
+			department_id, 
+			salary)
+		VALUES (OLD.first_name, OLD.last_name, OLD.middle_name, OLD.job_title, OLD.department_id, OLD.salary);
+		RETURN NEW;
+	END;
+$$ 
+LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER 
+	employee_delete_trigger
+AFTER DELETE ON 
+	employees
+FOR EACH ROW
+EXECUTE FUNCTION 
+	on_employee_delete();
