d3ea2485838375f4506de8454dd7afcd64b9e996
Borislav Ivanov <slambeca@abv.bg>
2023-10-12 18:37:41 +0300

    Update Exam Preparation III.sql

--- 8.3. Exam Preparation III/Exam Preparation III.sql
+++ 8.3. Exam Preparation III/Exam Preparation III.sql
@@ -336,3 +336,68 @@ $$
 LANGUAGE plpgsql;
 
 -- 4.12. Search for Board Games
+
+CREATE TABLE search_results (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50),
+    release_year INT,
+    rating FLOAT,
+    category_name VARCHAR(50),
+    publisher_name VARCHAR(50),
+    min_players VARCHAR(50),
+    max_players VARCHAR(50)
+);
+
+CREATE OR REPLACE PROCEDURE
+	usp_search_by_category(
+	IN category VARCHAR(50))
+AS
+$$
+	BEGIN
+		TRUNCATE TABLE search_results;
+		
+		INSERT INTO search_results (
+			"name",
+			release_year,
+			rating,
+			category_name,
+			publisher_name,
+			min_players,
+			max_players)
+		SELECT
+			bg.name,
+			bg.release_year,
+			bg.rating,
+			cat.name AS category_name,
+			pb.name AS publisher_name,
+			pr.min_players || ' people',
+			pr.max_players || ' people'
+		FROM
+			board_games AS bg
+		JOIN
+			categories AS cat
+		ON
+			bg.category_id = cat.id
+		JOIN
+			publishers AS pb
+		ON
+			bg.publisher_id = pb.id
+		JOIN
+			players_ranges AS pr
+		ON
+			bg.players_range_id = pr.id
+		WHERE
+			cat.name = category
+		ORDER BY
+			pb.name ASC,
+			bg.release_year DESC;
+	END;
+$$
+LANGUAGE plpgsql;
+
+CALL usp_search_by_category('Wargames')
+
+SELECT
+	*
+FROM
+	search_results;
