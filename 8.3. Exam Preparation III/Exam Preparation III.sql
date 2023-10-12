1bc0b99a241f05491dad542775a44f5b191b3b76
Borislav Ivanov <slambeca@abv.bg>
2023-10-12 14:24:32 +0300

    Update Exam Preparation III.sql

--- 8.3. Exam Preparation III/Exam Preparation III.sql
+++ 8.3. Exam Preparation III/Exam Preparation III.sql
@@ -1,3 +1,5 @@
+-- 1. 1. Database Design
+
 DROP TABLE IF EXISTS categories CASCADE;
 
 CREATE TABLE categories (
@@ -129,3 +131,185 @@ CREATE TABLE creators_board_games (
 	ON UPDATE CASCADE
 	ON DELETE CASCADE
 );
+
+-- 2.2. Insert
+
+INSERT INTO
+	board_games(name, release_year, rating, category_id, publisher_id, players_range_id)
+VALUES
+	('Deep Blue', 2019,	5.67, 1, 15, 7),
+	('Paris', 2016, 9.78, 7, 1, 5),
+	('Catan: Starfarers', 2021, 9.87, 7, 13, 6),
+	('Bleeding Kansas',	2020, 3.25,	3, 7, 4),
+	('One Small Step', 2019, 5.75, 5, 9, 2);
+	
+INSERT INTO
+	publishers(name, address_id, website, phone)
+VALUES
+	('Agman Games',	5, 'www.agmangames.com', '+16546135542'),
+	('Amethyst Games',	7,	'www.amethystgames.com', '+15558889992'),
+	('BattleBooks',	13,	'www.battlebooks.com', '+12345678907');
+
+-- 2. 3. Update
+
+UPDATE
+	players_ranges
+SET
+	max_players = max_players + 1
+WHERE
+	min_players = 2
+		AND
+	max_players = 2;
+	
+UPDATE
+	board_games
+SET
+	name = name || ' V2'
+WHERE
+	release_year >= 2020;
+
+-- 2.4. Delete
+
+DELETE 
+FROM
+	board_games
+WHERE
+	publisher_id IN (SELECT id FROM publishers WHERE address_id IN (SELECT id FROM addresses WHERE town LIKE 
+
+'L%'));
+	
+DELETE
+FROM
+	publishers
+WHERE
+	address_id IN (SELECT id FROM addresses WHERE town LIKE 'L%');
+
+DELETE
+FROM
+	addresses
+WHERE
+	town LIKE 'L%';
+
+-- 3.5. Board Games by Release Year
+
+SELECT
+	name,
+	rating
+FROM
+	board_games
+ORDER BY
+	release_year ASC,
+	name DESC;
+
+-- 3.6. Board Games by Category
+
+SELECT
+	bg.id,
+	bg.name,
+	release_year,
+	c.name
+FROM
+	board_games AS bg
+JOIN
+	categories AS c
+ON
+	bg.category_id = c.id
+WHERE
+	c.name IN ('Strategy Games', 'Wargames')
+ORDER BY
+	release_year DESC;
+
+-- 3.7. Creators without Board Games
+
+SELECT
+	c.id,
+	CONCAT_WS(' ', c.first_name, c.last_name) AS creator_name,
+	c.email
+FROM
+	creators AS c
+LEFT JOIN
+	creators_board_games AS cbg
+ON
+	c.id = cbg.creator_id
+WHERE
+	cbg.creator_id IS NULL
+ORDER BY
+	creator_name ASC;
+
+-- 3.8. First 5 Board Games
+
+SELECT
+	bg.name,
+	bg.rating,
+	c.name AS category_name
+FROM
+	board_games AS bg
+JOIN
+	categories AS c
+ON
+	bg.category_id = c.id
+JOIN
+	players_ranges AS pr
+ON
+	bg.players_range_id = pr.id
+WHERE
+	(bg.rating > 7.00 AND bg.name ILIKE '%a%')
+		OR
+	(bg.rating > 7.50 AND pr.min_players >= 2 AND pr.max_players <= 5)
+ORDER BY
+	bg.name ASC,
+	bg.rating DESC
+LIMIT 
+	5;
+
+-- 3.9. Creators with Emails
+
+SELECT
+    CONCAT_WS(' ', c.first_name, c.last_name) AS full_name,
+    c.email,
+    MAX(bg.rating) AS rating
+FROM
+    creators c
+JOIN
+    creators_board_games AS cbg 
+ON 
+	c.id = cbg.creator_id
+JOIN
+    board_games AS bg 
+ON 
+	cbg.board_game_id = bg.id
+WHERE
+    c.email LIKE '%.com'
+GROUP BY
+	c.first_name,
+	c.last_name,
+	c.email
+ORDER BY
+    full_name ASC;
+
+-- 3.10. Creators by Rating
+
+SELECT
+	c.last_name,
+	CEIL(AVG(bg.rating)) AS average_rating,
+	p.name AS publisher_name
+FROM
+	creators AS c
+JOIN
+	creators_board_games AS cbg
+ON
+	c.id = cbg.creator_id
+JOIN
+	board_games AS bg
+ON
+	cbg.board_game_id = bg.id
+JOIN
+	publishers AS p
+ON
+	bg.publisher_id = p.id
+WHERE
+	p.name = 'Stonemaier Games'
+GROUP BY
+	c.last_name, p.name
+ORDER BY
+	average_rating DESC;
